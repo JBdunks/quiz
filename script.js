268d53aca80f7d05a03d9fed304b67f16f1a060b
Jon Butler <Jwb2472@gmail.com>
2020-02-09 12:48:29 -0500

    question card

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 /*timer
  */
-var secondsLeft = 60;
+var secondsLeft = 5;
 countDown.textContent = secondsLeft;
 
 var start = document.getElementById("startButton");
 
 start.addEventListener("click", function() {
-  var secondsLeft = 61;
+  var secondsLeft = 5;
   var timerInterval = setInterval(function() {
     secondsLeft--;
     countDown.textContent = secondsLeft;
+    document.getElementById("startButton").style.visibility = "hidden";
 
     if (secondsLeft === 0) {
       clearInterval(timerInterval);
       alert("Time has run out.  Game over");
+      document.getElementById("startButton").style.visibility = "visible";
     }
   }, 1000);
 });
+
+/* questions
+ */
+var quest = [];
